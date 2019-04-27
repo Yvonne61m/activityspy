49c721abdda06de3615b6bbaa4c018864869d5e6
Yvonne61m <yvonne61m@gmail.com>
2019-04-26 18:40:57 -0700

    add all activity logs

--- app/src/main/java/edu/washington/yimengl/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/yimengl/activityspy/MainActivity.kt
@@ -5,18 +5,43 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = javaClass.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.i(javaClass.name,  "onCreate event fired")
+        Log.i(TAG,  "onCreate event fired")
         if (savedInstanceState != null) {
-            Log.i(javaClass.name, savedInstanceState.toString())
+            Log.i(TAG, savedInstanceState.toString())
         }
     }
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart event fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        Log.e(javaClass.name, "We’re going down, Captain!")
+        Log.e(TAG, "We’re going down, Captain!")
     }
 }
